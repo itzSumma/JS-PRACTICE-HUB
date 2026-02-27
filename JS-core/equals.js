c7049adc70370d894df72ec42f2ec4be2a58e63f
itzSumma <itzSumma11@gmail.com>
2026-02-27 15:10:21 +0600

    add double equal and strict equal

---
+++ JS-core/equals.js
@@ -0,0 +1,22 @@
+    // Compares the data types
+console.log(2 === 2);
+console.log(2 === 4);
+console.log(2 === "2");
+ 
+// Compares the   
+console.log(2==2);
+console.log(2==4);
+console.log(2=="2");
+      // TRUE 
+console.log(1==true);
+console.log(0==false);
+console.log(false =="0");
+console.log(true=="1"); 
+console.log(null == undefined);
+console.log([5]=="5");
+console.log([5].toString())
+
+        //FALSE
+ console.log(NaN==NaN);     
+ console.log({}=={});     
+ console.log([]==[]);     
