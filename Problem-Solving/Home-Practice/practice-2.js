b5df508cf3df46b7c56e73c178b6ca790e155677
itzSumma <itzSumma11@gmail.com>
2026-03-09 22:34:18 +0600

    second problem solved

--- Problem-Solving/Home-Practice/practice-2.js
+++ Problem-Solving/Home-Practice/practice-2.js
@@ -5,9 +5,19 @@ const text = "javascript";
 let reversed = "";
 // loop from last character
 for (let i = text.length - 1; i >= 0; i--) {
-
   // add characters one by one
   reversed = reversed + text[i];
-
+}console.log(reversed);
+// problem:2
+// word এর মধ্যে কতটি vowel (a,e,i,o,u) আছে তা বের করো।
+const word = "programming";
+const vowels = "aeiou";
+let count = 0;
+// loop through characters
+for (let char of word) {
+  // check if vowel
+  if (vowels.includes(char)) {
+    count++;
+  }
 }
-console.log(reversed);
+console.log(count);
