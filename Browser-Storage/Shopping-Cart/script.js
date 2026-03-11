abea9c6a948cf16562e212bade25a016c8d7a2ca
itzSumma <itzSumma11@gmail.com>
2026-03-11 09:05:48 +0600

    save in local storage

--- Browser-Storage/Shopping-Cart/script.js
+++ Browser-Storage/Shopping-Cart/script.js
@@ -6,7 +6,8 @@ const handleAddProduct =()=>{
 const productElement =document.getElementById("product");
 const quantityElement = document.getElementById("quantity");
 const product =productElement.value;
-const quantity = quantityElement.value;
+// const quantity = quantityElement.value;
+const quantity = parseInt(quantityElement.value);
 // console.log("add product",product,quantity)
 
 showProduct(product,quantity);
@@ -19,13 +20,34 @@ quantityElement.value="";
 
 //empty cart
 const getCart = ()=>{
-    const cart = {};
+    let cart = {};
+
+    //stored in LS
+    const cartJSON = localStorage.getItem("cart")
+    // console.log(cartJSON)
+if(cartJSON){
+    cart = JSON.parse(cartJSON);
+
+}
+
     return cart;
 }
 const addPoductToCart =(product,quantity)=>{
     const cart = getCart();
+
+      // if existing
+if(cart[product]){
+cart[product] += quantity
+} else{
     cart[product] = quantity;
+
+}
+
     console.log("cart", cart)
+
+    //Convert to json
+const cartJSON = JSON.stringify(cart);
+localStorage.setItem("cart", cartJSON)
 }
 
 const showProduct =(product,quantity)=>{
@@ -37,8 +59,31 @@ ul.appendChild(li)
 
 }
 
-/**
- * 1.To save object/array in the LS 
+
+ /**
+  * * Display product from local storage
+  */
+
+const displayStoredProducts = () =>{
+    const cart = getCart();
+for(let product in cart){
+    const quantity = cart[product]
+    console.log(product,quantity)
+    showProduct(product,quantity)
+}
+
+}
+displayStoredProducts()
+
+ /*
+  * 1.To save object/array in the LS 
  * 2.Convert the object to JSON  string by using JSON.stringify
  * 3.localStorage.setItem()
+ */
+
+/**
+ ** 1.To get object/array from the local storage  
+
+ * *2.Get the item from the local storage & it will be in json string
+ **3.Convert the JSON string to JS obj by using JSON.parse
  */
