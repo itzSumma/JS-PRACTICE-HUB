334c1526da5471968dca3d97128d68453d9a1cb9
itzSumma <itzSumma11@gmail.com>
2026-03-17 18:54:13 +0600

    find most frequent element in an array

--- Problem-Solving/Logic-Build/DAY-1.js
+++ Problem-Solving/Logic-Build/DAY-1.js
@@ -1,6 +1,6 @@
 /*
 *Find two numbers that add up to a target.(Input: "listen", "silent")
-*ANS
+*ANS:
 */
 function isAnagram(str1, str2) {
   let s1 = str1.split("").sort().join("");
@@ -9,4 +9,24 @@ function isAnagram(str1, str2) {
 }
 console.log(isAnagram("listen", "silent")); 
 
+/*
+*Find the number that appears most times in an array.
+* Input: [1, 2, 2, 3, 3, 3, 4]
+*ANS:
+*/
+function mostFrequent(arr) {
+  let count = {};
+  let maxCount = 0;
+  let result;
+  for (let num of arr) {
+    count[num] = (count[num] || 0) + 1;
+
+    if (count[num] > maxCount) {
+      maxCount = count[num];
+      result = num;
+    }
+  }
+  return result;
+}
+console.log(mostFrequent([1,2,2,3,3,3,4])); 
 
