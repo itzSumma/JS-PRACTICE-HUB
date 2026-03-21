ebae21ed01511157642595ae5bdf02ed3cd5f01f
itzSumma <itzSumma11@gmail.com>
2026-03-21 11:31:57 +0600

    Implement removeFalsy function using Array.filter and Boolean

--- Problem-Solving/Logic-Build/DAY-2.js
+++ Problem-Solving/Logic-Build/DAY-2.js
@@ -33,3 +33,10 @@ function charCount(str) {
 }
 
 console.log(charCount("aabbc"));
+/**
+ * *Remove all falsy values from an array.
+ */
+function removeFalsy(arr) {
+  return arr.filter(Boolean);
+}
+console.log(removeFalsy([0, 1, false, 2, "", 3, null, "hello"]));
