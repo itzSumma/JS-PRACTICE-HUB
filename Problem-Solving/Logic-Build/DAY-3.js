c4183c17869b8b619c47a737d59edb01d74a428f
itzSumma <itzSumma11@gmail.com>
2026-05-10 20:08:57 +0600

    check a prime number

--- Problem-Solving/Logic-Build/DAY-3.js
+++ Problem-Solving/Logic-Build/DAY-3.js
@@ -13,3 +13,23 @@ function factorial(num) {
 }
 
 console.log(factorial(5));
+
+/*
+ * Check whether a number is prime or not.
+ */
+
+function isPrime(num) {
+  if (num <= 1) {
+    return "Not Prime";
+  }
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return "Not Prime";
+    }
+  }
+
+  return "Prime";
+}
+
+console.log(isPrime(7));
